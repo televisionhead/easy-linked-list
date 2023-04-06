c9b4d9a288233548dd7ae23c32a344255ab33d4e
televisionhead <myles19098@gmail.com>
2023-04-06 14:25:54 -0400

    quickstart code

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include "list.h"
 
-/*
-
-TO DO: 
-
-1. split list at certain index ( will be used like this: list secondlist = mylist.split(&mylist); )
-2. merge sort a linked list ( will call split with index = midpoint )
-
-*/
-
 int main() {
+	//get a list
 	list mylist = list_create();
 
+	//define some variables
+	int first = 10;
+	char* second = "test";
+	float third = 100.420f;
+
+	//add them to the list
+	mylist.add(&mylist, &first, INTEGER);
+	mylist.add(&mylist, &second, STRING);
+	mylist.add(&mylist, &third, DECIMAL_F);
+
+	//remove an item
+	mylist.remove(&mylist, &first)
+
+	//print list contents
+	mylist.print(&mylist);
+
+	//destroy list when done
 	mylist.destroy(&mylist);
-	return 0;
+    return 0;
 }
