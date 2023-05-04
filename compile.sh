d6375c19c1dc2754c76a025e4723ac9eeb970030
televisionhead <myles19098@gmail.com>
2023-05-04 02:04:50 -0400

    adding -DNDEBUG version

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 gcc main.c list.c -o main -static -Ofast -Wall -Wextra
+
+#Use this one for compiling without the assert() statements
+# gcc main.c list.c -o main -static -Ofast -Wall -Wextra -DNDEBUG
